4c6548c19a5f6b4be128ff4319575aa350d965d6
DimbelMe <dimbel.me@gmail.com>
2026-05-31 19:58:31 -0400

    title

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
-const dateInput = document.getElementById("fecha-agenda");
-const today = new Date().toISOString().split("T")[0];
-dateInput.min = today;
-dateInput.value = today;
+document.addEventListener("DOMContentLoaded", function() {
+    const dateInput = document.getElementById("date");
+    const today = new Date().toISOString().split("T")[0];
+    dateInput.min = today;
+    dateInput.value = today;
+});
+
+function submitForm(event) {
+    event.preventDefault();
+    alert("¡Cita agendada con éxito!");
+    document.getElementById("appointment-form").reset();
+}
